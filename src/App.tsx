1e930ca21a5b548b76524f246759881ff153cf2a
Modin Sergei <jetleebruce@gmail.com>
2023-03-04 20:35:22 +0300

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import  AppComponent from '../src/components/app-header/app-header';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <AppComponent/>
-    </div>
+    </>
   );
 }
 
