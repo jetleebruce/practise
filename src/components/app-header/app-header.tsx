16fa80c49f6bcd3879b47d647b50e49afb8889dd
Modin Sergei <jetleebruce@gmail.com>
2023-03-08 19:11:54 +0300

    create app-header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -2,7 +2,7 @@ import {
   BurgerIcon,
   ListIcon,
   Logo,
-  ProfileIcon
+  ProfileIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import headertStyles from "./app-header.module.css";
 
@@ -10,28 +10,31 @@ const AppHeader = () => {
   return (
     <header className={headertStyles.header}>
       <div className={headertStyles.innerBox}>
-        <div className={headertStyles.burger}>
-          <BurgerIcon type="primary" />
-          <span className={headertStyles.burgertext}>
-            <p className="text text_type_main-default">Конструктор</p>
-          </span>
-        </div>
-        <div className={headertStyles.order}>
-          <ListIcon type="secondary" />
-          <span className={headertStyles.ordertext}>
-            <p className="text text_type_main-default">Лента заказов</p>
-          </span>
+        <div className={headertStyles.buttonswrap}>
+          <div className={headertStyles.burger}>
+            <BurgerIcon type="primary" />
+            <span className={headertStyles.burgertext}>
+              <p className="text text_type_main-default">Конструктор</p>
+            </span>
+          </div>
+          <div className={headertStyles.order}>
+            <ListIcon type="secondary" />
+            <span className={headertStyles.ordertext}>
+              <p className="text text_type_main-default">Лента заказов</p>
+            </span>
+          </div>
         </div>
         <div className={headertStyles.logowrap}>
+          <div>
             <Logo />
+          </div>
         </div>
         <div className={headertStyles.roomwrap}>
-        <ProfileIcon type="secondary" />
-        <span className={headertStyles.roomtext}>
+          <ProfileIcon type="secondary" />
+          <span className={headertStyles.roomtext}>
             <p className="text text_type_main-default">Личный кабинет</p>
-        </span>
+          </span>
         </div>
-        <div></div>
       </div>
     </header>
   );
