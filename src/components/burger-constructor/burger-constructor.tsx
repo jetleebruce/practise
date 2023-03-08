9d472ff2ab8c60fe86bf84bfaf2e5513d06555a8
Modin Sergei <jetleebruce@gmail.com>
2023-03-08 19:12:48 +0300

    create burger-constructor

---
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -0,0 +1,9 @@
+import constructorStyles from './burger-constructor.module.css';
+
+const BurgerConstructor = () => {
+    return (
+        <div className={constructorStyles.blockwrap}>BurgerConstructor</div>
+    )
+};
+
+export default BurgerConstructor;
