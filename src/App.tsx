76b6c36d1d1720db8dc3c7cab91f6c1cdca715e6
Modin Sergei <jetleebruce@gmail.com>
2023-03-08 19:11:03 +0300

    structure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import  AppComponent from '../src/components/app-header/app-header';
+import AppHeader from "../src/components/app-header/app-header";
+import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
+import BurgerConstructor from "./components/burger-constructor/burger-constructor";
+import { data } from "./utils/data";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [ingerdients] = useState(data);
   return (
-    <>
-      <AppComponent/>
-    </>
+    <div>
+      <AppHeader />
+      <main className="boxwrap">
+        <div className="innerbox">
+        <BurgerIngredients data={ingerdients}/>
+        <BurgerConstructor />
+        </div>
+      </main>
+    </div>
   );
 }
 
