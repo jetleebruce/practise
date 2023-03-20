d03af9e30cc9854cbbbe1a5e3daf1053c2ae97b5
Modin Sergei <jetleebruce@gmail.com>
2023-03-20 20:06:28 +0300

    refactor burger-constructor

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,10 +1,8 @@
 import constructorStyles from "./burger-constructor.module.css";
-import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
-import bunUp from "../../images/bun-02.png";
-import sause from "../../images/sauce-03.png";
-import meat from "../../images/meat-02.png";
-import sp from "../../images/sp.png";
-import mineral from "../../images/mineral-rings.png";
+import {
+  ConstructorElement,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import substruct from "../../images/subtract.png";
 import { Ingredient } from "../../App";
 
@@ -13,60 +11,69 @@ interface Props {
 }
 
 const BurgerConstructor = ({ arrIngred }: Props) => {
-
   const getBunsInfo = arrIngred.find((ingredient) => {
     return ingredient.type === "bun";
   });
+
+  const selectedProducts: Ingredient[] = [];
+  while (selectedProducts.length < 5) {
+    const randomIndex = Math.floor(Math.random() * arrIngred.length);
+    const randomProduct = arrIngred[randomIndex];
+    if (
+      !selectedProducts.includes(randomProduct) &&
+      randomProduct.type !== "bun"
+    ) {
+      selectedProducts.push(randomProduct);
+    }
+  }
 
   return (
     <div className={constructorStyles.blockwrap}>
       <div className={constructorStyles.innerBox}>
         <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
           {getBunsInfo && (
-            <ConstructorElement
-              type="top"
-              isLocked={true}
-              text={`${getBunsInfo.name} (верх)`}
-              price={getBunsInfo.price}
-              thumbnail={getBunsInfo.image}
-            />
+            <div className={constructorStyles.card}>
+              <span style={{ visibility: "hidden" }}>
+                <DragIcon type="primary" />
+              </span>
+              <ConstructorElement
+                type="top"
+                isLocked={true}
+                text={`${getBunsInfo.name} (верх)`}
+                price={getBunsInfo.price}
+                thumbnail={getBunsInfo.image}
+              />
+            </div>
           )}
           <div className={constructorStyles.parent}>
             <div className={constructorStyles.child}>
-              <ConstructorElement
-                text="Соус традиционный галактический"
-                price={50}
-                thumbnail={sause}
-              />
-              <ConstructorElement
-                text="Мясо бессмертных моллюсков Protostomia"
-                price={50}
-                thumbnail={meat}
-              />
-              <ConstructorElement
-                text="Плоды Фалленианского дерева"
-                price={50}
-                thumbnail={sp}
-              />
-              <ConstructorElement
-                text="Хрустящие минеральные кольца"
-                price={50}
-                thumbnail={mineral}
-              />
-              <ConstructorElement
-                text="Хрустящие минеральные кольца"
-                price={50}
-                thumbnail={mineral}
-              />
+              {selectedProducts.map((product) => (
+                <div className={constructorStyles.card}>
+                  <DragIcon type="primary" />
+                  <ConstructorElement
+                    key={product._id}
+                    text={product.name}
+                    price={product.price}
+                    thumbnail={product.image}
+                  />
+                </div>
+              ))}
             </div>
           </div>
-          <ConstructorElement
-            type="bottom"
-            isLocked={true}
-            text="Краторная булка N-200i (низ)"
-            price={200}
-            thumbnail={bunUp}
-          />
+          {getBunsInfo && (
+            <div className={constructorStyles.card}>
+              <span style={{ visibility: "hidden" }}>
+                <DragIcon type="primary" />
+              </span>
+              <ConstructorElement
+                type="top"
+                isLocked={true}
+                text={`${getBunsInfo.name} (низ)`}
+                price={getBunsInfo.price}
+                thumbnail={getBunsInfo.image}
+              />
+            </div>
+          )}
         </div>
       </div>
       <div className={constructorStyles.order}>
